5d866f588d95d04a003bd6f5fb547e4a654a9e3d
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-06-17 20:25:54 -0600

    Initial Setup

--- src/InfolistCarouselServiceProvider.php
+++ src/InfolistCarouselServiceProvider.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Tuto1902\InfolistCarousel;
 
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Tuto1902\InfolistCarousel\Commands\InfolistCarouselCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class InfolistCarouselServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +18,7 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->name('infolist-carousel')
+            ->hasViews();
     }
 }
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Tuto1902\InfolistCarousel\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
